23c4bcf62592f672c8295e3b478fec01ff74ffab
finma <firmanmaulana2403@gmail.com>
2021-12-24 14:32:43 +0700

    build: contact page

---
+++ src/pages/contact/index.tsx
@@ -0,0 +1,10 @@
+import Layout from '@/components/layout/Layout';
+import { Contact } from '@/components/sections';
+
+export default function AboutPage() {
+  return (
+    <Layout>
+      <Contact />
+    </Layout>
+  );
+}
