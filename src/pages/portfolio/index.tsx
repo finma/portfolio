7faea79ac3abeb0de1398b1c874c76d9f69a890d
finma <firmanmaulana2403@gmail.com>
2021-12-24 13:34:42 +0700

    build: portfolio page

---
+++ src/pages/portfolio/index.tsx
@@ -0,0 +1,10 @@
+import Layout from '@/components/layout/Layout';
+import { Portfolio } from '@/components/sections';
+
+export default function PortfolioPage() {
+  return (
+    <Layout>
+      <Portfolio />
+    </Layout>
+  );
+}
