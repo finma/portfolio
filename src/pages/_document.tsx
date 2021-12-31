093af42915a465114b3403003faf54ac92a319fe
finma <firmanmaulana2403@gmail.com>
2021-12-31 09:21:00 +0700

    update: optimized font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-document-import-in-page */
 import Document, {
   DocumentContext,
   Head,
@@ -23,6 +24,16 @@ class MyDocument extends Document {
             type='font/woff2'
             crossOrigin='anonymous'
           />
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link
+            rel='preconnect'
+            href='https://fonts.gstatic.com'
+            crossOrigin='anonymous'
+          />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Spartan:wght@300;400;500;600;700&display=swap'
+            rel='stylesheet'
+          ></link>
         </Head>
         <body>
           <Main />
