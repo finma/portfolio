97288c8d5a08ef02dad68420cbe06b7822e9a126
finma <firmanmaulana2403@gmail.com>
2024-07-04 09:46:27 +0700

    refactor: get passions from data

--- src/data/data.ts
+++ src/data/data.ts
@@ -123,3 +123,23 @@ export const works = [
     tech: ['ReactJS', 'Firebase', 'React Chat Engine'],
   },
 ];
+
+export const passionItems = [
+  {
+    title: 'Coding',
+    icon: '/icons/coding.svg',
+    description:
+      'I started like coding since I was in college. From there I learned programming languages and structural algorithms. I had a hard time at first, but when I tried again and again, I finally love it',
+  },
+  {
+    title: 'Technology',
+    icon: '/icons/cpu.svg',
+    description:
+      "Technology is the sum of every technique, skill, method, and process used in the production of goods or services. Over time, technology is developing. I'm very interested in technology, I can't wait to see what technology will look like in the future",
+  },
+  {
+    title: 'Learn New Things',
+    icon: '/icons/learning.svg',
+    description: `“The more that you read, the more things you will know. The more that you learn, the more places you'll go.” - Dr. Seuss`,
+  },
+];
