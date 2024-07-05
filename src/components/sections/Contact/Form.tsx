2efe5410451c2a125023a7fe693719df4bd2d2d2
finma <firmanmaulana2403@gmail.com>
2024-07-05 09:16:35 +0700

    add: submit form to google sheets

--- src/components/sections/Contact/Form.tsx
+++ src/components/sections/Contact/Form.tsx
@@ -1,6 +1,37 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import { FormEvent, useRef, useState } from 'react';
+import { toast } from 'sonner';
+
 import Button from '@/components/buttons/Button';
 
 export const Form = () => {
+  const scriptURL = process.env.NEXT_PUBLIC_GOOGLE_SHEETS_API_URL!;
+
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    setIsLoading(true);
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+
+    fetch(scriptURL, { method: 'POST', body: formData })
+      .then((response) => {
+        if (response.ok) {
+          toast.success('Your message has been sent!');
+          formRef.current?.reset();
+        } else {
+          toast.error('Your message has not been sent!');
+        }
+      })
+      .catch((error) => console.error('Error sending message!', error))
+      .finally(() => setIsLoading(false));
+  };
+
   return (
     <div
       data-aos='fade'
@@ -8,7 +39,7 @@ export const Form = () => {
       data-aos-duration='1000'
       className='flex justify-center items-start px-8 w-full md:p-0 lg:items-center lg:w-1/2'
     >
-      <form className='w-full max-w-lg'>
+      <form onSubmit={onSubmit} ref={formRef} className='w-full max-w-lg'>
         <div className='flex flex-wrap -mx-3 mb-2 lg:mb-6'>
           <div className='px-3 mb-2 w-full md:mb-0 md:w-1/2'>
             <label
@@ -18,8 +49,9 @@ export const Form = () => {
               Name
             </label>
             <input
-              className='border-red-custom block px-4 py-3 mb-3 w-full leading-tight text-white bg-transparent rounded border-2 appearance-none focus:bg-transparent focus:outline-none'
+              className='border-red-custom caret-red-custom block px-4 py-3 mb-3 w-full leading-tight text-white bg-transparent rounded border-2 appearance-none focus:bg-transparent focus:outline-none'
               id='name'
+              name='name'
               type='text'
               placeholder='Name'
               required
@@ -33,8 +65,9 @@ export const Form = () => {
               Email
             </label>
             <input
-              className='border-red-custom block px-4 py-3 mb-3 w-full leading-tight text-white bg-transparent rounded border-2 appearance-none focus:bg-transparent focus:outline-none'
+              className='border-red-custom caret-red-custom block px-4 py-3 mb-3 w-full leading-tight text-white bg-transparent rounded border-2 appearance-none focus:bg-transparent focus:outline-none'
               id='email'
+              name='email'
               type='email'
               placeholder='Email'
               required
@@ -45,13 +78,14 @@ export const Form = () => {
           <div className='px-3 w-full'>
             <label
               className='block mb-2 text-sm font-bold tracking-wide text-white'
-              htmlFor='password'
+              htmlFor='subject'
             >
               Subject
             </label>
             <input
-              className='border-red-custom block px-4 py-3 mb-3 w-full leading-tight text-white bg-transparent rounded border-2 appearance-none focus:bg-transparent focus:outline-none'
-              id='password'
+              className='border-red-custom caret-red-custom block px-4 py-3 mb-3 w-full leading-tight text-white bg-transparent rounded border-2 appearance-none focus:bg-transparent focus:outline-none'
+              id='subject'
+              name='subject'
               type='text'
               placeholder='Subject'
               required
@@ -67,15 +101,18 @@ export const Form = () => {
               Message
             </label>
             <textarea
-              className='border-red-custom block px-4 py-3 mb-3 w-full leading-tight text-white bg-transparent rounded border-2 appearance-none focus:bg-transparent focus:outline-none'
+              className='border-red-custom caret-red-custom block px-4 py-3 mb-3 w-full h-32 leading-tight text-white bg-transparent rounded border-2 appearance-none resize-none focus:bg-transparent focus:outline-none'
               id='message'
-              rows={4}
+              name='message'
               placeholder='Message'
+              rows={4}
               required
-            ></textarea>
+            />
           </div>
         </div>
-        <Button variant='primary'>Send message!</Button>
+        <Button type='submit' variant='primary' isLoading={isLoading}>
+          Send message!
+        </Button>
       </form>
     </div>
   );
