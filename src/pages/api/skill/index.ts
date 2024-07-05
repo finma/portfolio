5967e607ca55445b781eeb4436387d7e5c158343
finma <firmanmaulana2403@gmail.com>
2024-07-05 20:38:33 +0700

    update: remove folder api

--- src/pages/api/skill/index.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-import { skillItems } from '@/data/data';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json(skillItems);
-}
--- src/pages/api/work/index.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-import { portfolios } from '@/data/data';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json(portfolios);
-}
