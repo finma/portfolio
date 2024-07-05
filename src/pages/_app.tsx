a473b2a2ad5e21d74b68287a2f657f587f72d75a
finma <firmanmaulana2403@gmail.com>
2024-07-05 09:05:06 +0700

    add: toaster sonner

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import AOS from 'aos';
 import { AppProps } from 'next/app';
 import { useEffect } from 'react';
+import { Toaster } from 'sonner';
 
 import 'aos/dist/aos.css';
 import '@/styles/globals.css';
@@ -14,7 +15,12 @@ function MyApp({ Component, pageProps }: AppProps) {
     });
   });
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Toaster />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
