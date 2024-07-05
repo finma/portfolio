0ef7b751d841491021c1ba755b7ebada263d224f
finma <firmanmaulana2403@gmail.com>
2024-07-05 21:18:49 +0700

    add: new skill set

--- src/components/sections/AboutDetail/SkillSet.tsx
+++ src/components/sections/AboutDetail/SkillSet.tsx
@@ -17,14 +17,13 @@ export const SkillSet = () => {
           <span className='text-red-custom font-semibold'>03</span> Skill Set
         </h3>
       </div>
-      <div className='flex flex-wrap gap-10 justify-center items-center m-auto mt-0 w-full h-full md:w-3/4 lg:flex-row lg:justify-evenly lg:items-start lg:mt-20 lg:w-2/3'>
-        {skills.map(({ title, icon }, index) => (
+      <div className='flex flex-wrap gap-10 justify-center items-center m-auto mt-0 w-full h-full lg:flex-row lg:justify-evenly lg:items-start lg:mt-20'>
+        {skills.map(({ title, icon }) => (
           <div
             key={title}
             data-aos='fade-up'
             data-aos-easing='easeInOutCubic'
             data-aos-duration='1000'
-            data-aos-delay={index * 50}
           >
             <CardSkill title={title} icon={icon} />
           </div>
--- src/data/skill.ts
+++ src/data/skill.ts
@@ -1,35 +1,55 @@
 export const skills = [
   {
-    title: 'MongoDB',
-    icon: '/icons/skills/mongodb.svg',
-  },
-  {
-    title: 'ExpressJS',
-    icon: '/icons/skills/expressjs.svg',
-  },
-  {
-    title: 'React',
-    icon: '/icons/skills/reactjs.svg',
-  },
-  {
-    title: 'NodeJS',
-    icon: '/icons/skills/nodejs.svg',
+    title: 'Javascript',
+    icon: '/icons/skills/javascript.svg',
   },
   {
     title: 'Typescript',
     icon: '/icons/skills/typescript.svg',
   },
   {
-    title: 'Javascript',
-    icon: '/icons/skills/javascript.svg',
+    title: 'React',
+    icon: '/icons/skills/reactjs.svg',
+  },
+  {
+    title: 'NextJS',
+    icon: '/icons/skills/nextjs.svg',
+  },
+  {
+    title: 'ExpressJS',
+    icon: '/icons/skills/expressjs.svg',
+  },
+  {
+    title: 'NestJS',
+    icon: '/icons/skills/nestjs.svg',
+  },
+  {
+    title: 'NodeJS',
+    icon: '/icons/skills/nodejs.svg',
+  },
+  {
+    title: 'MongoDB',
+    icon: '/icons/skills/mongodb.svg',
   },
   {
     title: 'MySQL',
     icon: '/icons/skills/mysql.svg',
   },
   {
-    title: 'Laravel',
-    icon: '/icons/skills/laravel.svg',
+    title: 'Prisma',
+    icon: '/icons/skills/prisma.svg',
+  },
+  {
+    title: 'Dart',
+    icon: '/icons/skills/dart.svg',
+  },
+  {
+    title: 'Flutter',
+    icon: '/icons/skills/flutter.svg',
+  },
+  {
+    title: 'Redux',
+    icon: '/icons/skills/redux.svg',
   },
   {
     title: 'TailwindCSS',
@@ -44,7 +64,7 @@ export const skills = [
     icon: '/icons/skills/php.svg',
   },
   {
-    title: 'NextJS',
-    icon: '/icons/skills/nextjs.svg',
+    title: 'Laravel',
+    icon: '/icons/skills/laravel.svg',
   },
 ];
