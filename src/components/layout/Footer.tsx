a973bdbc24bdae0fc2c3d3118f8976bc0c1ea6f6
finma <firmanmaulana2403@gmail.com>
2024-07-05 21:34:14 +0700

    update: footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,10 +1,16 @@
+import Link from 'next/link';
+
 export const Footer = () => {
   const year = new Date().getFullYear();
 
   return (
     <div className='w-full h-24 bg-transparent lg:h-12'>
       <p className='text-sm text-center text-white'>
-        &copy;{year} FINMA. All Right Reserved
+        &copy;{year}{' '}
+        <Link href='https://github.com/finma'>
+          <a className='hover:text-red-custom'>FINMA.</a>
+        </Link>{' '}
+        All Right Reserved
       </p>
     </div>
   );
